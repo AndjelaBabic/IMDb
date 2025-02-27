fe3e79d74e92b25e157672181beae08f2341acb6
AndjelaBabic <andjelasbabic@gmail.com>
2025-02-27 22:20:22 +0100

    Sort movies by imdb rating

--- src/MovieGrid.tsx
+++ src/MovieGrid.tsx
@@ -7,7 +7,11 @@ import { Movie } from "./types";
 export const MovieGrid = () => {
   const [favorites, setFavorites] = useState<Record<number, boolean>>({});
 
-  const movies: Movie[] = moviesData.slice(0, 12);
+  const movies: Movie[] = moviesData.sort((a, b) => {
+    const ratingA = a.ratings.find((r) => r.id === "imdb")?.rating || 0;
+    const ratingB = b.ratings.find((r) => r.id === "imdb")?.rating || 0;
+    return ratingB - ratingA;
+  });
 
   const toggleFavorite = (id: number) => {
     setFavorites((prev) => ({ ...prev, [id]: !prev[id] }));
